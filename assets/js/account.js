c3556571508574a8f541c2174a2c6b51b00379b7
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-11-07 02:09:35 +0330

    bootstarp, scss for css, images, font family & js

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,23 +1,34 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const text = document.getElementById('text');
-const link = document.getElementById('link');
 const btn = document.getElementById('btn');
+const text2 = document.getElementById('text-2');
+const btn2 = document.getElementById('btn-2');
 const users = [
-    { email: 'rexvero@gmail.com', password: '123456' }
-]
+    { email: localStorage.getItem("email"), password: localStorage.getItem("pass") },
+];
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     if (email.value === "" && password.value === "") {
-        text.innerText = "Please Enter Your Email & Password";
+        text.innerText = "Please Enter Your Email";
+        text2.innerText = "Please Enter Your Password";
     } else if (password.value !== "") {
         text.innerText = "Please Enter Your Email";
+        text2.innerText = "";
     } else if (email.value !== "") {
-        text.innerText = "Please Enter Your Password";
+        text.innerText = "";
+        text2.innerText = "Please Enter Your Password";
     }
     if (email.value !== "" && password.value !== "") {
-        text.innerText = "Incrroct inputs!";
+        text.innerText = "";
+        text2.innerText = "";
+        btn.innerText = "Done";
+        localStorage.setItem("email", email.value);
+        localStorage.setItem("pass", password.value);
     }
+});
+btn2.addEventListener('click', (e) => {
+    e.preventDefault();
     users.filter(item => {
         if (email.value === item.email && password.value === item.password) {
             window.location.replace("../index.html");
