fa8866f90cb6e74e47f8ed0d5de9a74836a1707a
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-11-16 01:14:04 +0330

    scss for css & js

---
+++ assets/js/index.js
@@ -0,0 +1,85 @@
+const arrow = document.getElementById('arrow');
+const arrow2 = document.getElementById('arrow2');
+const sun = document.getElementById('sun');
+const font = document.getElementById('font');
+const moon = document.getElementById('moon');
+const home = document.getElementById('home');
+const html = document.getElementById('html');
+const brand = document.getElementById('brand');
+const languages = document.getElementById('Languages');
+const languages2 = document.getElementById('Languages2');
+const list = document.getElementById('list');
+const drop = document.getElementById('drop');
+arrow.addEventListener('click', () => {
+    arrow.style.display = 'none';
+    arrow2.style.display = 'block';
+});
+arrow2.addEventListener('click', () => {
+    arrow.style.display = 'block';
+    arrow2.style.display = 'none';
+});
+english.addEventListener('click', () => {
+    html.setAttribute('lang', 'en');
+    html.setAttribute('dir', 'ltr');
+    font.classList.add('LibreBaskerville');
+    font.classList.remove('BMitraBd');
+    arrow.style.display = 'block';
+    arrow2.style.display = 'none';
+    home.innerText = 'Account';
+    languages.innerText = 'Languages';
+    languages2.innerText = 'Languages';
+    persian.innerText = 'Persian';
+    english.innerText = 'English';
+
+});
+persian.addEventListener('click', () => {
+    html.setAttribute('lang', 'fa');
+    html.setAttribute('dir', 'rtl');
+    font.classList.add('BMitraBd');
+    font.classList.remove('LibreBaskerville');
+    arrow.style.display = 'block';
+    arrow2.style.display = 'none';
+    home.innerText = 'حساب کاربری';
+    languages.innerText = 'زبان ها';
+    languages2.innerText = 'زبان ها';
+    persian.innerText = 'فارسی';
+    english.innerText = 'انگلیسی';
+});
+sun.addEventListener('click', () => {
+    english.classList.add('item2');
+    english.classList.remove('item');
+    persian.classList.add('item2');
+    persian.classList.remove('item');
+    list.classList.add('color2');
+    list.classList.remove('color');
+    drop.classList.add('menu2');
+    drop.classList.remove('menu');
+    sun.style.display = 'none';
+    moon.style.display = 'block';
+    moon.classList.add('hover');
+    moon.classList.remove('hover2');
+    home.classList.add('hover');
+    arrow.classList.add('hover');
+    arrow2.classList.add('hover');
+    brand.classList.add("brand2");
+    brand.classList.remove("brand");
+});
+moon.addEventListener('click', () => {
+    english.classList.add('item');
+    english.classList.remove('item2');
+    persian.classList.add('item');
+    persian.classList.remove('item2');
+    list.classList.add('color');
+    list.classList.remove('color2');
+    drop.classList.add('menu');
+    drop.classList.remove('menu2');
+    moon.style.display = 'none';
+    sun.style.display = 'block';
+    moon.classList.add('hover2');
+    moon.classList.remove('hover');
+    arrow.classList.remove('hover');
+    arrow2.classList.remove('hover');
+    home.classList.remove('hover');
+    brand.classList.add("brand");
+    brand.classList.remove("brand2");
+});
